7d4dcd7365627ca37221257f932bad190a5f8943
Jaisse <jaisse@gmail.com>
2023-05-09 20:35:43 -0300

    completados os passos dos cenarios da feature compra

--- features/environment.py
+++ features/environment.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 
 # Inicio
 def before_all(context): # Antes de tudo
-
     #Declarar o Selenium, instanciar como o navegador e apontar o driver
     context.driver = webdriver.Chrome('C:/Users/jaiss_f3yllmx/PycharmProjects/fts132_blazedemo/drivers/chrome/96/chromedriver.exe')
-
-
-    #Maximizar a janela do navegador
     context.driver.maximize_window()
 
+    print('Passo A - antes de tudo')
 #Fim
 def after_all(context): #depois de tudo
 
     #Desligar / Destruir o objeto do selenium
     context.driver.quit()
 
-    print('Passo 7 - Depois de Tudo')
+    print('Passo Z - Depois de Tudo')
+#BLOCO EXECUTADO AI FINAL DE CADA step
+def after_step(context, step):
+    print()
+
--- features/steps/Compra.py
+++ features/steps/Compra.py
@@ -1,6 +1,7 @@
 import time
 
 from behave import given, when, then
+from selenium import select
 from selenium import *
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -19,7 +20,7 @@ def before_feature(context, feature):
 def step_impl(context):
     context.driver.get('https://www.blazedemo.com')
     print('Passo 1 - Acessou o site Blazedemo')
-    time.sleep(15)  #espera bruta - sempre remover - alfinete
+    time.sleep(5)  #espera bruta - sempre remover - alfinete
 
 
 @when(u'seleciono a cidade de origem como "{origem}"')
@@ -33,7 +34,8 @@ def step_impl(context, origem):
 
     #Seleciona o elemento no combo
     objeto_origem.select_by_visible_text(origem)
-    # objeto_origem.select_by_value(origem)
+    #objeto_origem.select_by_value(origem)
+
 
     print('Passo 2 - Selecionou a cidade de origem')
 
@@ -41,54 +43,81 @@ def step_impl(context, origem):
 def step_impl(context, destino):
 
     #mapeia o combo com as cidades de origem
-    combo_origem = context.driver.find_element(By.NAME, 'toPort')
+    combo_destino = context.driver.find_element(By.NAME, 'toPort')
 
     #cria um objeto para permitir selecionar as opcoes do combo
-    objeto_origem = Select(combo_origem)
+    objeto_destino = Select(combo_destino)
 
     #Seleciona o elemento no combo
-    objeto_origem.select_by_visible_text(destino)
+    objeto_destino.select_by_visible_text(destino)
     # objeto_origem.select_by_value(origem)
-
-
     print('Passo 3 - Selecionou a cidade de destino')
+    #time.sleep(3)
 
-
-@when(u'clico em "Find Flights"')
+@when(u'clico no botao "Find Flights"')
 def step_impl(context):
-    print('Passo 4- When clico em Find Flights')
+    context.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary').click()
+    print('Passo 4- When clico no botao Find Flights')
 
 
 @then(u'sou direcionado para a pagina de selecao de voos')
 def step_impl(context):
+    assert context.driver.find_element(By.TAG_NAME, 'h3').text() == 'Flights from São Paolo to Rome:'
     print('Passo 5 - direcionou para a pagina de selecao de voos')
 
 
 @when(u'seleciono o primeiro voo')
 def step_impl(context):
+    context.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-small').click()
     print('Passo 6 - selecionou o primeiro voo')
 
 
 @then(u'sou direcionado para a pagina de pagamento')
 def step_impl(context):
+    context.driver.find_element(By.XPATH, "//p[contains(text(),'please submit the form below to purchase the flight.')]").text == 'Please submit the form below to purchase the flight.'
     print('Passo 7 - direcionou para a pagina de pagamento')
 
 
 @when(u'preencho os dados de pagamento')
 def step_impl(context):
+    context.driver.find_element(By.ID, 'inputName').send_keys('James Bond')
     print('Passo 8 - Preencheu os dados para pagamento')
 
 
 @when(u'clico no botao Purchase Flight')
 def step_impl(context):
+    context.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary').click()
     print('Passo 9 -clico no botao Purchase Flight')
 
 
 @then(u'sou direcionado para a pagina de confirmacao')
 def step_impl(context):
+    assert context.driver.find_element (By.TAG_NAME, 'h1').text() == 'Thank you for purchasetoday!'
     print('Passo 10 - Then sou direcionado para a pagina de confirmacao')
 
 
-@when(u'seleciono de "São Paolo" para "Rome"')
-def step_impl(context):
+@when(u'seleciono de "{origem}" para "{destino}"')
+def step_impl(context, origem, destino):
+    # mapeia o combo com as cidades de origem
+    combo_origem = context.driver.find_element(By.NAME, 'fromPort')
+
+    # cria um objeto para permitir selecionar as opcoes do combo
+    objeto_origem = Select(combo_origem)
+
+    # Seleciona o elemento no combo
+    objeto_origem.select_by_visible_text(origem)
+    # objeto_origem.select_by_value(origem)
+
+    #mapeia o combo com as cidades de origem
+    combo_destino = context.driver.find_element(By.NAME, 'toPort')
+
+    #cria um objeto para permitir selecionar as opcoes do combo
+    objeto_destino = Select(combo_destino)
+
+    #Seleciona o elemento no combo
+    objeto_destino.select_by_visible_text(destino)
+
+    context.driver.find_element(By.CSS_SELECTOR, 'input.btn.btn-primary').click()
+
     print('Passo 2c- Selecionou a cidade de origem e destino')
+
